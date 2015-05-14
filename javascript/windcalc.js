90ab6b68c8bd09dab4dc4d9f214c7ef82cb09f60
Rory Mearns <r.mearnsy@gmail.com>
2015-05-14 16:09:28 +1200

    added a bunch of comments

--- javascript/windcalc.js
+++ javascript/windcalc.js
@@ -1,26 +1,26 @@
 /* JavaScript/jQuery for windsurf calculator website */
 
-var weightKG = 85, 
-weightAlfa = 2.20462262185,  // kg-lb conversion factor
-sailAlpha = 1.34,
-finAlpha = 4.9383,
-finExtra = 3.0988,
-skilled = "selected", 
-learner = "not_selected"
-freeride = "selected",
-wave = "not_selected",
-customWind1 = false,
-customWind2 = false,
-customWind3 = false;
+var weightKG = 85,            // rider weight, default is 85kg
+weightAlfa = 2.20462262185,   // kg-lb conversion factor
+sailAlpha = 1.34,             // to calculate the recommended sail
+finAlpha = 4.9383,            // to calculate the recommended fin
+finExtra = 3.0988,            // also used to calculate the recomended fin
+skilled = "selected",         // user selected skill level
+learner = "not_selected"      // user selected skill level
+freeride = "selected",        // user riding style selection
+wave = "not_selected",        // user riding style selection
+customWind1 = false,          // user set wind speed 1
+customWind2 = false,          // user set wind speed 2
+customWind3 = false;          // user set wind speed 3
 
 // Function to update the DOM
 function update() {
   $('#weight_lb').val((weightKG * weightAlfa).toFixed(0));
   $('#weight_kg').val((weightKG*1).toFixed(0));
-  
   var weightFactor = sailAlpha*weightKG
   var finFactor = 1.34*weightKG;
 
+  // Set all the default sail size suggestions
   $("#sail10").html((weightFactor/10).toFixed(1));
   $("#sail15").html((weightFactor/15).toFixed(1));
   $("#sail20").html((weightFactor/20).toFixed(1));
@@ -28,10 +28,12 @@ function update() {
   $("#sail30").html((weightFactor/30).toFixed(1));
   $("#sail35").html((weightFactor/35).toFixed(1));
 
+  // Set the custom sail size suggestions
   customWind1 ? ($("#c1wind").html(customWind1), $("#c1sail").html((weightFactor/customWind1).toFixed(1))) : '';
   customWind2 ? ($("#c2wind").html(customWind2), $("#c2sail").html((weightFactor/customWind2).toFixed(1))) : '';
   customWind3 ? ($("#c3wind").html(customWind3), $("#c3sail").html((weightFactor/customWind3).toFixed(1))) : '';
   
+  // Set the default fin size suggestions
   $("#fin10").html((finAlpha*(finFactor/10)+finExtra).toFixed(0));
   $("#fin15").html((finAlpha*(finFactor/15)+finExtra).toFixed(0));
   $("#fin20").html((finAlpha*(finFactor/20)+finExtra).toFixed(0));
@@ -39,15 +41,15 @@ function update() {
   $("#fin30").html((finAlpha*(finFactor/30)+finExtra).toFixed(0));
   $("#fin35").html((finAlpha*(finFactor/35)+finExtra).toFixed(0));
 
+  // Set the custom fin size suggestions
   customWind1 ? $("#c1fin").html((finAlpha*(finFactor/customWind1)+finExtra).toFixed(0)) : '';
   customWind2 ? $("#c2fin").html((finAlpha*(finFactor/customWind2)+finExtra).toFixed(0)) : '';
   customWind3 ? $("#c3fin").html((finAlpha*(finFactor/customWind3)+finExtra).toFixed(0)) : '';
 
+  // Clear custom settings if removed
   ($("#custom1").val() == '') ? ($("#c1wind").html(''), $("#c1sail").html(''), $("#c1fin").html('')) : '';
   ($("#custom2").val() == '') ? ($("#c2wind").html(''), $("#c2sail").html(''), $("#c2fin").html('')) : '';
   ($("#custom3").val() == '') ? ($("#c3wind").html(''), $("#c3sail").html(''), $("#c3fin").html('')) : '';
-
-  
 };
 
 // Make changes depending on the skill level selected by the user
@@ -79,9 +81,6 @@ function adjustStyle () {
   update();
 };
 
-function customWind () {
-
-};
 
 /* --- Function to listen for changes --- */
 $(document).ready(function() {
