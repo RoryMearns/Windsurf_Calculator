95b6f8921ddbee2c26e18fd2574c71d010c427c6
Rory Mearns <r.mearnsy@gmail.com>
2015-05-14 15:52:09 +1200

    added custom wind setting functions

--- javascript/windcalc.js
+++ javascript/windcalc.js
@@ -8,30 +8,49 @@ finExtra = 3.0988,
 skilled = "selected", 
 learner = "not_selected"
 freeride = "selected",
-wave = "not_selected";
+wave = "not_selected",
+customWind1 = false,
+customWind2 = false,
+customWind3 = false;
 
-/* --- Function to update the DOM --- */
+// Function to update the DOM
 function update() {
   $('#weight_lb').val((weightKG * weightAlfa).toFixed(0));
   $('#weight_kg').val((weightKG*1).toFixed(0));
   
   var weightFactor = sailAlpha*weightKG
+  var finFactor = 1.34*weightKG;
+
   $("#sail10").html((weightFactor/10).toFixed(1));
   $("#sail15").html((weightFactor/15).toFixed(1));
   $("#sail20").html((weightFactor/20).toFixed(1));
   $("#sail25").html((weightFactor/25).toFixed(1));
   $("#sail30").html((weightFactor/30).toFixed(1));
   $("#sail35").html((weightFactor/35).toFixed(1));
+
+  customWind1 ? ($("#c1wind").html(customWind1), $("#c1sail").html((weightFactor/customWind1).toFixed(1))) : '';
+  customWind2 ? ($("#c2wind").html(customWind2), $("#c2sail").html((weightFactor/customWind2).toFixed(1))) : '';
+  customWind3 ? ($("#c3wind").html(customWind3), $("#c3sail").html((weightFactor/customWind3).toFixed(1))) : '';
   
-  var finFactor = 1.34*weightKG;
   $("#fin10").html((finAlpha*(finFactor/10)+finExtra).toFixed(0));
   $("#fin15").html((finAlpha*(finFactor/15)+finExtra).toFixed(0));
   $("#fin20").html((finAlpha*(finFactor/20)+finExtra).toFixed(0));
   $("#fin25").html((finAlpha*(finFactor/25)+finExtra).toFixed(0));
   $("#fin30").html((finAlpha*(finFactor/30)+finExtra).toFixed(0));
   $("#fin35").html((finAlpha*(finFactor/35)+finExtra).toFixed(0));
+
+  customWind1 ? $("#c1fin").html((finAlpha*(finFactor/customWind1)+finExtra).toFixed(0)) : '';
+  customWind2 ? $("#c2fin").html((finAlpha*(finFactor/customWind2)+finExtra).toFixed(0)) : '';
+  customWind3 ? $("#c3fin").html((finAlpha*(finFactor/customWind3)+finExtra).toFixed(0)) : '';
+
+  ($("#custom1").val() == '') ? ($("#c1wind").html(''), $("#c1sail").html(''), $("#c1fin").html('')) : '';
+  ($("#custom2").val() == '') ? ($("#c2wind").html(''), $("#c2sail").html(''), $("#c2fin").html('')) : '';
+  ($("#custom3").val() == '') ? ($("#c3wind").html(''), $("#c3sail").html(''), $("#c3fin").html('')) : '';
+
+  
 };
 
+// Make changes depending on the skill level selected by the user
 function adjustSkill () {
   var temp = skilled;
   skilled = learner;
@@ -42,6 +61,7 @@ function adjustSkill () {
   update();
 };
 
+// Make changes depending on the style selected by user
 function adjustStyle () {
   var temp = freeride;
   freeride = wave;
@@ -57,7 +77,11 @@ function adjustStyle () {
   $("#freeride").attr('class', freeride);
   $("#wave").attr('class', wave);
   update();
-}
+};
+
+function customWind () {
+
+};
 
 /* --- Function to listen for changes --- */
 $(document).ready(function() {
@@ -81,4 +105,18 @@ $(document).ready(function() {
   $("#wave").click(function() {
     (wave == "selected") ? update() : adjustStyle();
   });
+  $("#custom1").change(function() {
+    customWind1 = $("#custom1").val();
+    update();
+  });
+  $("#custom2").change(function() {
+    customWind2 = $("#custom2").val();
+    update();
+  });
+  $("#custom3").change(function() {
+    customWind3 = $("#custom3").val();
+    update();
+  });
 });
+
+update();
